6d1080fa866e4afb229a0cc2017b6666e1fb0f81
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-07-07 13:55:51 +0900

    Create MemberService.java

---
+++ 01.hello/src/main/java/com/work/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.work.service;
+
+import java.util.Map;
+
+public class MemberService {
+
+	public static void addMember(Map<String, String> dtoMap) {
+		System.out.println("service addMember() : " + dtoMap);
+		
+	}
+
+}
